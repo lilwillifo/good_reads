194eda136acce08e99c22b5397356198c1202134
lilwillifo <margaretwilliford4@gmail.com>
2018-03-30 18:29:52 -0600

    model test for average review

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,4 +7,15 @@ describe Book, type: :model do
       expect(book).to respond_to(:reviews)
     end
   end
+  describe 'instance methods' do
+    it '.average_review' do
+      book = Book.create!(title: 'Murder on the Orient Express')
+      user_1 = User.create!(name: 'Margaret')
+      user_2 = User.create!(name: 'Ali')
+      review_1 = book.reviews.create!(body: 'great read!', user_id: user_1.id, rating: 5)
+      review_2 = book.reviews.create!(body: 'meh', user_id: user_2.id, rating: 3)
+
+      expect(book.average_review).to eq(4)
+    end
+  end
 end
